18a44acd533919df596366107d11a833b3e9a32c
Alina Soomro <alina.soomro@yahoo.com>
2025-03-12 22:23:57 +0500

    updates

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,32 +2,49 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import HeaderSocials from './HeaderSocials'
+import { BsLinkedin } from 'react-icons/bs'
+import { BsGithub } from 'react-icons/bs'
 
 const Header = () => {
   return (
-    
-    
-      <header id='home'>
 
-        <div className="container header__container">
 
-          <h5>Hello, I'm</h5>
-          <h1>Alina Soomro</h1>
-          <h5 className="text-light">Full Stack Developer</h5>
-          <CTA/>
-          <HeaderSocials/>
+    <header id='home'>
+      {/* profile picture  */}
+      {/* <div className='home__image' >
+        <img src="assets/Popup-Login-Page-Example.jpg" />
+      </div> */}
+      {/*  profile picture */}
+      <div className="container header__container">
 
-          {/* <div className="me">
+        <h5>Hello, I'm</h5>
+        <h1>Alina Soomro</h1>
+        <h5 className="text-light">Full Stack Developer</h5>
+        <p>
+          I'm a <b>Full Stack Developer and MS Software Engineering Student</b>.
+
+
+        </p>
+
+        <CTA />
+
+        {/* <HeaderSocials /> */}
+        <div className='socials__header'>
+          <div className='socials__header_a'><a href="https://www.linkedin.com/in/alina-mehdi-59308413a" target="_blank" rel='noreferrer'><BsLinkedin /></a></div>
+          <div className='socials__header_a'><a href="https://github.com/AlinaS012" target="_blank" rel='noreferrer'><BsGithub /></a></div>
+
+        </div>
+        {/* <div className="me">
 
             <img src={me} alt='me'/>
           </div> */}
 
-          <a href="#contacts" className='scroll__down'>Scroll Down</a>
-        </div>
-      </header>
+        <a href="#contacts" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
 
-    
-    
+
+
   )
 }
 
--- src/components/myprojects/Myprojects.jsx
+++ src/components/myprojects/Myprojects.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './myprojects.css'
+import { FiExternalLink } from 'react-icons/fi'
+import { BsGithub } from 'react-icons/bs'
 // import PropertyImg from './assets/Screenshot (19).png'
 // import MqttDashboard from './assets/Screenshot (20).png'
 // import NativeProject from './assets/project.jpeg'
 const Myprojects = () => {
-  return (
+  return (<>
     <section id='myprojects'>
 
       <h5>My Recent Works</h5>
@@ -13,44 +15,76 @@ const Myprojects = () => {
       <div className="container portfolio__container">
 
         <article className='portfolio__item'>
-            <div className="portfolio__item-image">
-                <img src={'assets/img1.png'//PropertyImg
-                } alt="" />
-            </div>
-            <h3>Property Search App</h3>
-            <small className='text-light'>React | Nest.js</small>
-            <div className="portfolio__item-cta">
-            </div>
-              
-        </article>
-        <article className='portfolio__item'>
-            <div className="portfolio__item-image">
-                <img src={"assets/img3.png"//MqttDashboard
-                } alt="" />
-            </div>
-            <h3>IOT Project Admin Dashboard</h3>
-            <small className='text-light'>React | MUI | Node.js | AWS</small>
-            <div className="portfolio__item-cta">
-            </div>
-            
-        </article>
-        <article className='portfolio__item'>
-            <div className="portfolio__item-image">
-                <img src='assets/project.jpeg'//NativeProject
+          <div className="portfolio__item-image">
+            <img src='assets/todolist.png'//NativeProject
 
-                 alt="" style={{ height: '20%'}} />
-            </div>
-            <h3>Bluetooth Tracking App</h3>
-            <small className='text-light'>ReactNative | Bluetooth API</small>
-            <div className="portfolio__item-cta">
-            </div>
-            
+              alt="" style={{ height: '20%' }} />
+          </div>
+          <h3>MUI TodoList</h3>
+          <small className='text-light'>React | MUI</small>
+          <div className="portfolio__item-cta">
+            {/* <div className="header__socials"> */}
+
+            <a href="https://taupe-maamoul-032134.netlify.app/" target="_blank" rel='noreferrer'><FiExternalLink /></a>
+            <a href="https://github.com/AlinaS012/MUI-todolist" target="_blank" rel='noreferrer'><BsGithub /></a>
+            {/* </div> */}
+          </div>
+
         </article>
-        
-        
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src='assets/static-ecommerce.png'//NativeProject
+
+              alt="" style={{ height: '20%' }} />
+          </div>
+          <h3>Static Shoe ecommerce site</h3>
+          <small className='text-light'>React | Bootstrap</small>
+          <div className="portfolio__item-cta">
+
+            <a href="https://unrivaled-pasca-f3ffc0.netlify.app/" target="_blank" rel='noreferrer'><FiExternalLink /></a>
+            <a href="https://github.com/AlinaS012/Stylish-Store" target="_blank" rel='noreferrer'><BsGithub /></a>
+          </div>
+
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={'assets/img1.png'//PropertyImg
+            } alt="" />
+          </div>
+          <h3>Property Search App</h3>
+          <small className='text-light'>React | Nest.js</small>
+          <div className="portfolio__item-cta">
+          </div>
+
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={"assets/img3.png"//MqttDashboard
+            } alt="" />
+          </div>
+          <h3>IOT Project Admin Dashboard</h3>
+          <small className='text-light'>React | MUI | Node.js | AWS</small>
+          <div className="portfolio__item-cta">
+          </div>
+
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src='assets/project.jpeg'//NativeProject
+
+              alt="" style={{ height: '20%' }} />
+          </div>
+          <h3>Bluetooth Tracking App</h3>
+          <small className='text-light'>ReactNative | Bluetooth API</small>
+          <div className="portfolio__item-cta">
+          </div>
+
+        </article>
+
+
       </div>
     </section>
-  )
+  </>)
 }
 
 export default Myprojects
